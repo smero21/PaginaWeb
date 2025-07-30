4e4a0bad16c4f83ee864daae1b3d0117e44c9c89
Sebastian Mendez <sebastianmendez0806@gmail.com>
2025-07-30 23:06:59 +0300

    Actualización del navbar, subida de imagenes para la pagina home y creación del componente footer

--- PaginaWeb/src/app/paginas/home/home.ts
+++ PaginaWeb/src/app/paginas/home/home.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../../componentes/nav-bar/nav-bar';
+import { Card } from '../../componentes/card/card';
+import { Footer } from '../../componentes/footer/footer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBar],
+  imports: [NavBar,
+    Card,
+    Footer,
+    CommonModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  images = [
+    "angular-original-wordmark.svg",
+    "docker-original-wordmark.svg",
+    "java-original-wordmark.svg",
+    "mysql-original-wordmark.svg",
+    "postgresql-original-wordmark.svg",
+    "postman-original.svg",
+    "python-original.svg",
+    "ibm--app-connect-enterprise.svg",
+    "ibm--mq.svg"
+  ]
 
 }
